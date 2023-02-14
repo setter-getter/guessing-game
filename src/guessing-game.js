872458203d965313d860584343dbe5acac305dbc
setter-getter <dimkinyashchik@yandex.ru>
2023-02-14 11:56:51 +0400

    feat: add GuessingGame class

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,29 @@
 class GuessingGame {
-    constructor() {}
-
+    constructor() {
+      this.min = null;
+      this.max = null;
+      this.guessNumber = null;
+    }
+    
     setRange(min, max) {
-
+      this.min = min;
+      this.max = max;
     }
-
+    
     guess() {
-
+      this.guessNumber = Math.round((this.min + this.max) / 2);
+      return this.guessNumber;
     }
-
+    
     lower() {
-
+      this.max = this.guessNumber;
     }
-
+    
     greater() {
-
+      this.min = this.guessNumber;
     }
-}
+  }
+  
 
 module.exports = GuessingGame;
+
